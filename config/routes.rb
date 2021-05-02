b592648bec9d368b51561f52d34d64c06a286b4a
korethisyo <korethis0816@gmail.com>
2021-05-02 08:28:30 +0000

    [Update]work3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  get 'home/about' => 'homes#about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  get 'home/about' => 'homes#about'
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
